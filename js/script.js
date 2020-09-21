530ad4269d1032c96fe857d70c03f2669972bc29
lapaduga <deniskotov29042015@gmail.com>
2020-09-22 03:29:18 +0500

    g

--- js/script.js
+++ js/script.js
@@ -14,4 +14,28 @@ $(document).ready(function () {
 		$(this).toggleClass('active');
 		return false;
 	});
+	//Form placeholders
+	$('#name, #email, #message').on('focus', function (e) {
+		$(this).attr('placeholder', '');
+	});
+	$('#name, #email, #message').on('blur', function (e) {
+		$('#name').attr('placeholder', 'Name / Company name');
+		$('#email').attr('placeholder', 'Email');
+		$('#message').attr('placeholder', 'Your message');
+	});
+	$('#name, #email, #message').on('keyup', function (e) {
+		if ($(this).val() != '') {
+			$(this).css({
+				'border' : '2px solid',
+				'border-image' : 'linear-gradient(90deg, #6bb86c 0%, #0c988d 100%)',
+				'border-image-slice' : '1'
+			});
+		} else {
+			$(this).css({
+				'border' : '1px solid',
+				'border-image' : 'linear-gradient(90deg, #6bb86c 0%, #0c988d 100%)',
+				'border-image-slice' : '1'
+			});
+		}
+	});
 });
